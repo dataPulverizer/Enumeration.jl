46422690027db5b4399fdc67c0ffb2e672551cf5
dataPulverizer <data.pulverizer@gmail.com>
2017-12-10 18:30:17 +0000

    Initial Enumeration.jl commit

---
+++ Enumeration.jl
@@ -0,0 +1,51 @@
+#==
+  C++ class style enums for Julia, written in Julia v0.7.0-DEV
+  Created 10th December 2017
+==#
+
+abstract type Enumeration{T} end
+
+function Enumerate(name::Symbol, data::Dict{T, Int})::Void where T
+  enumExpression = quote
+    struct $name{T} <: Enumeration{T}
+      value::Int
+      data::Dict{T, Int}
+      function $name(x::T) where T
+        value::Int = $data[x]
+        new{T}(value, $data)
+      end
+    end
+  end
+
+   #==
+     There is a default equality expression, this one overrides it.
+   ==#
+
+   equalityExpression = quote
+     import Base.==
+     ==(x::$name, y::$name) = x.value == y.value
+   end
+
+   eval(Main, enumExpression)
+   eval(Main, equalityExpression)
+   return
+end
+
+#== Example:
+
+julia> include("Enumeration.jl")
+
+julia> Enumerate(:Fruit, Dict("apple" => 1, "orange" => 2, "pear" => 3, "guava" => 4))
+
+julia> myFruit = Fruit("guava")
+Fruit(1, Dict("guava"=>4,"orange"=>2,"pear"=>3,"apple"=>1))
+
+julia> yourFruit = Fruit("pear")
+Fruit(3, Dict("guava"=>4,"orange"=>2,"pear"=>3,"apple"=>1))
+
+julia> yourFruit == myFruit
+false
+
+julia> myFruit == Fruit("guava")
+true
+==#
